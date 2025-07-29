ead7b5bcd2d4558815734b8a158546d77803850e
okayu <yuto.gio.0515@gmail.com>
2025-07-29 10:24:29 +0900

    コントローラー・テンプレート修正

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,16 +2,23 @@ package com.example.demo.controller;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.service.ImageService;
+
 @Controller
 public class HelloController {
+    @Autowired
+    private ImageService imageService;
+
     @GetMapping({ "", "/" })
     public ModelAndView index(ModelAndView mav) {
         // 現在時刻を取得
@@ -27,8 +34,14 @@ public class HelloController {
             hostName = "Unknown";
         }
 
+        // 一時的にS3オブジェクトにアクセスできるURLを生成
+        String imageUrl1 = imageService.generatePresignedUrl("icon_bear.jpg", Duration.ofHours(1));
+        String imageUrl2 = imageService.generatePresignedUrl("sampledir/icon.jpeg", Duration.ofHours(1));
+
         mav.addObject("currentTime", currentTime);
         mav.addObject("hostName", hostName);
+        mav.addObject("imageUrl1", imageUrl1);
+        mav.addObject("imageUrl2", imageUrl2);
         mav.setViewName("hello");
         return mav;
     }
